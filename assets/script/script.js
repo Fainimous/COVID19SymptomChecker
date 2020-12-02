00f369850cfb1b7db6394f5099741627e530198e
Russell Jewett <rfjewett3@gmail.com>
2020-12-01 21:29:52 -0500

    Adds api fetch and symptoms array.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,19 +1,21 @@
 // global variables
 
-//BetterDoctor API query
-var queryURL = "https://api.betterdoctor.com/2016-03-01/"
-var apiKey = "ab008c398f6e29934b87e7fd85a69f06";
+// The Covid Tracking Project API query
+var state = "nc";
+var queryURL = "https://api.covidtracking.com/v1/states/" + state + "/current.json";
 
 
-//var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + api_key;
+//COVID-19 symptoms
+var symptoms = ["fever", " dry cough", "tiredness", "aches", "sore throat", "diarrhea", "conjunctivitis", "headache", "loss of taste or smell", "rash",
+    "discoloration of fingers or toes", "difficulty breathing or shortness of breath", "chest pain or pressure", "loss of speech of movement"]
 
-
-//var resource_url = 'https://api.betterdoctor.com/2016-03-01/practices?location=37.773,-122.413,10&skip=2&limit=10&user_key=' + api_key;
-
-
-    $.ajax({
-        url: queryURL + "doctors?location=28214&skip=2&limit=5&user_key=" + apiKey,
-        method: "GET"
-      }).then(function(response) {
-          console.log(response);
-      })
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+    console.log('State: ' + response.state);
+    console.log('Positive Cases: ' + response.positive + '.');
+    console.log('Negative Cases: ' + response.negative + '.');
+    console.log('Total Recovered: ' + response.recovered + '.');
+})
