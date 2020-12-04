eeb76767cd264bc6436210843c832af4fa3d7750
Christopher Fain <faintank@gmail.com>
2020-12-03 21:43:53 -0500

    Set the getCoordinates function to prompt to allow location and store the lat/log coords to localStorage.  Also adjusted the 2 functions to search nearby doctors and covid locations to get the coords from local storage and use them in the API call.

--- assets/script/script.js
+++ assets/script/script.js
@@ -21,9 +21,6 @@ $.ajax({
 })
 
 //google API query
-var pos = "35.227,-80.843";
-var doctorQueryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyD3cN9fFq2wZXBnBtB9pCu-nv72cNa4MVE&location=" + pos + "&keyword=doctors%20office&rankby=distance";
-var covidQueryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyD3cN9fFq2wZXBnBtB9pCu-nv72cNa4MVE&location=" + pos + "&keyword=covid%20testing&rankby=distance";
 
 //get lat/long coordinants of the user
 //this is getting the coords, but it doesnt seem to be saving the results to the global varibale "pos" I set above...
@@ -33,6 +30,7 @@ function getCoordinates() {
     if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
         pos = position.coords.latitude + "," + position.coords.longitude;
+        localStorage.setItem("position", JSON.stringify(pos));
     }, () => {
         // Browser supports geolocation, but user has denied permission
         console.log("user has denied permission for Geolocation");
@@ -44,19 +42,24 @@ function getCoordinates() {
 }
 
 
-
 // Perform a Places Nearby Search Request for doctors offices
 function getNearbyDoctorsOffice() {
-    $.ajax({
-        url: doctorQueryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    }) 
+    var pos = JSON.parse(localStorage.getItem("position"));
+    console.log(pos);
+    var doctorQueryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyD3cN9fFq2wZXBnBtB9pCu-nv72cNa4MVE&location=" + pos + "&keyword=doctors%20office&rankby=distance";
+    console.log(doctorQueryURL);
+//    $.ajax({
+//        url: doctorQueryURL,
+//        method: "GET"
+//    }).then(function (response) {
+//        console.log(response);
+//    }) 
 }
 
 // Perform a Places Nearby Search Request for Covid testing
 function getNearbyCovidTesting(position) {
+    var pos = JSON.parse(localStorage.getItem("position"));
+    var covidQueryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyD3cN9fFq2wZXBnBtB9pCu-nv72cNa4MVE&location=" + pos + "&keyword=covid%20testing&rankby=distance";
     $.ajax({
         url: covidQueryURL,
         method: "GET"
@@ -66,7 +69,8 @@ function getNearbyCovidTesting(position) {
 }
 
 // un-comment these to test :]
-getNearbyDoctorsOffice();
+//getNearbyDoctorsOffice();
 //getNearbyCovidTesting();
+//getCoordinates();
 
 
